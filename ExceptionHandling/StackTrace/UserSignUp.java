a127b8de0e93d911749008e56dca40dcf9351bc2
SadushaNadeesh <sadusha1994@gmail.com>
2021-01-18 19:35:28 +0530

    Exception Handling

--- ExceptionHandling/StackTrace/UserSignUp.java
+++ ExceptionHandling/StackTrace/UserSignUp.java
@@ -7,12 +7,18 @@ import UserSignUp.UserName;
  * @author Sadusha
  */
 public class UserSignUp {
-    public static void main(String[] args) {
+
+    public static void signUp(){
         try {
             UserName userName = new UserName();
             userName.validateUserName("Sadusha123");
         } catch (Exception e) {
             System.out.println("Exception caused by : "+e);
+            //System.err.println(e);
         }
+
+    }
+    public static void main(String[] args) {
+        signUp();
     }
 }
