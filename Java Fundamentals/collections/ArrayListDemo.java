2f371ad21c1859a1580b900bf5050c7800ac803e
SadushaNadeesh <sadusha1994@gmail.com>
2021-01-24 18:01:30 +0530

    Java Fundamentals

---
+++ Java Fundamentals/collections/ArrayListDemo.java
@@ -0,0 +1,45 @@
+package collections;
+
+import java.util.*;
+
+/**
+ *
+ * @author Sadusha
+ */
+// Java program to demonstrate the 
+// working of ArrayList in Java 
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        // Declaring the ArrayList with 
+        // initial size n 
+        ArrayList<Integer> al
+                = new ArrayList<Integer>();
+
+        // Appending new elements at 
+        // the end of the list 
+        for (int i = 1; i <= 5; i++) {
+            al.add(i);
+        }
+
+        // Printing elements 
+        System.out.println(al);
+
+        // Remove element at index 3 
+        al.remove(3);
+
+        // Displaying the ArrayList 
+        // after deletion 
+        System.out.println(al);
+
+        // Printing elements one by one 
+        for (int i = 0; i < al.size(); i++) {
+            System.out.print(al.get(i) + " ");
+        }
+    }
+}
+/*
+[1, 2, 3, 4, 5]
+[1, 2, 3, 5]
+1 2 3 5
+*/
---
+++ Java Fundamentals/collections/HashSetDemo.java
@@ -0,0 +1,36 @@
+package collections;
+// Java program to demonstrate the 
+// working of a HashSet 
+
+import java.util.*;
+
+/**
+ *
+ * @author Sadusha
+ */
+public class HashSetDemo {
+
+    public static void main(String args[]) {
+        // Creating HashSet and 
+        // adding elements 
+        HashSet<String> hs = new HashSet<String>();
+
+        hs.add("Geeks");
+        hs.add("For");
+        hs.add("Geeks");
+        hs.add("Is");
+        hs.add("Very helpful");
+
+        // Traversing elements 
+        Iterator<String> itr = hs.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
+/*
+Very helpful
+Geeks
+For
+Is
+*/
